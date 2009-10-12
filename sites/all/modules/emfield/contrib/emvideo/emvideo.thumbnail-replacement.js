eed3b6e4ae115cb16a152bcc4c1a78f3a88553d6
DrupalCamp Texas and DrupalCamp Austin <drupalcamp@web3.fourkitchens.com>
2009-10-12 03:49:42 +0000

    Updating Embedding Media Field module (TRN).

--- sites/all/modules/emfield/contrib/emvideo/emvideo.thumbnail-replacement.js
+++ sites/all/modules/emfield/contrib/emvideo/emvideo.thumbnail-replacement.js
@@ -1,19 +1,23 @@
-// $Id: emvideo.thumbnail-replacement.js,v 1.1.2.1 2009/02/26 03:35:54 aaron Exp $
+// $Id: emvideo.thumbnail-replacement.js,v 1.1.2.2 2009/09/01 20:35:24 aaron Exp $
 
 /**
  *  @file
  *  This will use jQuery AJAX to replace a thumbnail with its video version.
- *
- *  Drupal.settings.emvideo.thumbs will contain an array with the following:
- *
  */
-
-if (Drupal.jsEnabled) {
-  $(document).ready(function(){
+Drupal.behaviors.emvideoThumbnailReplacement = function(context) {
+  // On load, add a span for the play button to any required thumbnails.
+  $('a.emvideo-thumbnail-replacement:not(.emvideo-thumbnail-replacement-processed)', context).addClass('emvideo-thumbnail-replacement-processed').each(function() {
     if (Drupal.settings.emvideo.thumbnail_overlay) {
-      $(".emvideo-thumbnail-replacement").prepend("<span></span>");
+      // Add the play button.
+      $(this).prepend("<span></span>");
     }
-    $('.emvideo-thumbnail-replacement').click(function() {
+  });
+
+  // Inline videos will click through to the video.
+  $('a.emvideo-modal-emvideo:not(.emvideo-modal-emvideo-processed)', context).addClass('emvideo-modal-emvideo-processed').each(function() {
+    // When clicking the image, load the video with AJAX.
+    // Note that this only happens if this is not a lightbox.
+    $(this).click(function() {
       // 'this' won't point to the element when it's inside the ajax closures,
       // so we reference it using a variable.
       var element = this;
@@ -37,4 +41,4 @@ if (Drupal.jsEnabled) {
       return false;
     });
   });
-}
+};
