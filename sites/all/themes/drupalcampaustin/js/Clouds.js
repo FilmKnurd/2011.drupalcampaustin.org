7c1d1bb7a0176acb15879c505932fa8343b6469b
DrupalCamp Texas and DrupalCamp Austin <drupalcamp@web3.fourkitchens.com>
2010-10-14 19:54:34 +0000

    Even moar cloudz\!

---
+++ sites/all/themes/drupalcampaustin/js/Clouds.js
@@ -0,0 +1,28 @@
+
+var Cloud = function(opts) {
+  this.init(opts);
+};
+
+Cloud.prototype = {
+
+  init: function(opts) {
+    var self = this;
+    
+    this.image = new Image();
+    this.image.src = Math.round(Math.random() * 2)
+      ? "sites/all/themes/drupalcampaustin/images/cloud-left.png"
+      : "sites/all/themes/drupalcampaustin/images/cloud-right.png";
+    
+    this.image.onload = function() {
+      self.width = Math.round(self.image.width * Math.random());
+      self.height = Math.round(self.width * (self.image.height / self.image.width));
+      
+      self.rate = Math.round(self.height * .8 / 10);
+    };
+    
+    this.rate = Math.round(Math.random() + 1 * 4);
+    this.x = 0;
+    this.y = 0;
+  }
+  
+};
