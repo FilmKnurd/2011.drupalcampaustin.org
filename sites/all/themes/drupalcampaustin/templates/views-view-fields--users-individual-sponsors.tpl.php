99f533fed138c9d9cc7b06b1c27628dbbbba6ff0
DrupalCamp Texas and DrupalCamp Austin <drupalcamp@web3.fourkitchens.com>
2010-10-18 18:34:57 +0000

    Adding view of individual sponsors.

---
+++ sites/all/themes/drupalcampaustin/templates/views-view-fields--users-individual-sponsors.tpl.php
@@ -0,0 +1,29 @@
+<?php
+// $Id: views-view-fields.tpl.php,v 1.6 2008/09/24 22:48:21 merlinofchaos Exp $
+/**
+ * @file views-view-fields.tpl.php
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->separator: an optional separator that may appear before a field.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php foreach ($fields as $id => $field): ?>
+  <?php if ($id == 'field_user_picture_fid'): ?>
+    <div class="picture"><?php print l($field->content, 'user/' . $row->uid, array('html' => TRUE)); ?></div>
+  <?php elseif ($id == 'title'): ?>
+    <div class="profile-link"><?php print l($field->raw, 'user/' . $row->uid); ?></div>
+  <?php endif; ?>
+<?php endforeach; ?>
